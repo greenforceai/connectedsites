d6b74f8849f832213e05a57240eff1da4a759bba
mano@fieldgenie.com <mano@fieldgenie.com>
2024-04-28 16:51:33 +0000

    add custom doctypes

---
+++ connectedsites/connected_sites/doctype/connected_customer/connected_customer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Greenforce.ai and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ConnectedCustomer(Document):
+	pass
---
+++ connectedsites/connected_sites/doctype/connected_supplier/connected_supplier.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Greenforce.ai and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Connected Supplier", {
+// 	refresh(frm) {
+
+// 	},
+// });
